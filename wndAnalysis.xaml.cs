30f872693b2d2240657a1dd1466b8b8d8b98f417
werehampster <alex_c_stahl@hotmail.com>
2019-11-16 19:18:46 +0100

    trying to get wndAnalysis to work. Won't display the text file in the datagrid, logFile contains data from log file. It's a dummy .txt file with 4 lines right now

--- wndAnalysis.xaml.cs
+++ wndAnalysis.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +10,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -15,13 +19,68 @@ using System.Windows.Shapes;
 namespace AbschlussprojektWPF
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for wndAnalysis.xaml
     /// </summary>
     public partial class wndAnalysis : Window
     {
+        
+        
         public wndAnalysis()
         {
             InitializeComponent();
+            fillDataGrid();
+        }
+
+
+        private void fillDataGrid()
+        {
+            DataGridView dataGridView = new DataGridView();
+            System.Windows.Controls.DataGrid dataGrid = new System.Windows.Controls.DataGrid();
+            string[] logFile = System.IO.File.ReadAllLines(@"D:\KeysOnlyNoLogger.txt");
+
+            DataTable dataTable = new DataTable();
+            
+            dataGridView.DataSource = dataTable;
+            dgAnalysis.DataContext = dataTable;
+            dataGrid.ItemsSource = logFile;
+
+            dataTable.Columns.Add();
+            dataTable.Columns.Add();
+            dataTable.Columns.Add();
+            int k = 0;
+            while(k < logFile.Length -1 )
+            {
+                dataTable.Rows.Add();
+                k++;
+            }
+            for (int i = 0; i < logFile.Length - 1; i++)
+            {
+
+                dataGrid.ItemsSource = logFile[i];
+                
+                //dataGridView.Rows[i].SetValues(logFile[i]);   
+                //dataGridView.Rows[i] = logFile[i];
+            }
+            
+            
+            
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnEasyTask_Click(object sender, RoutedEventArgs e)
+        {
+            // nur starten wenn ET noch nicht läuft
+            Process.Start(@"C:\Program Files\EasyTask\EasyTask.exe");
         }
     }
 }
