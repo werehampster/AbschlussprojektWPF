2584d30fa023f73727847149d4a47eb7ce5182c3
werehampster <alex_c_stahl@hotmail.com>
2020-01-02 14:43:14 +0100

    can log key combinations now

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,6 +16,9 @@ namespace AbschlussprojektWPF
     /// </summary>
     public partial class App : Application
     {
+        string Key = "";
+        string SecondKey = "";
+        Logger activeWindow = new Logger();
         public Logger LogFile { get; set; }
 
         public static bool IsLoggingStarted { get; set; }
@@ -30,19 +33,33 @@ namespace AbschlussprojektWPF
 
                     if (IsLoggingStarted)
                     {
-                        ////////////////////// Logik für Filter kommt hierhin //////////////////////////////
-                        //Tab
-                        //Enter
-                        //Space
-                        // I need focused element, because I don't want to log when in textfield
-                        // I need to know which program is used, because I'll have dfferent filters (for example word vs firefox)
-                        Logger activeWindow = new Logger();
-                        File.AppendAllText(@"D:\KeysOnly.txt", character.KeyCode + "\r\n");
-                        File.AppendAllText(@"D:\ActiveProgram.txt", activeWindow.GetTitleOfActiveWindow() + "\r\n");
 
+                            ////////////////////// Logik für Filter kommt hierhin //////////////////////////////
+                            //Tab
+                            //Enter
+                            //Space
+                            // I need focused element, because I don't want to log when in textfield
+                            // I need to know which program is used, because I'll have dfferent filters (for example word vs firefox)
+
+
+                        if (character.KeyCode.ToString() == "Insert")
+                        {
+                            Key = "NVDA + ";
+                            File.AppendAllText(@"D:\KeysOnly.txt", Key + " ");
+                            File.AppendAllText(@"D:\ActiveProgram.txt", activeWindow.GetTitleOfActiveWindow() + "\r\n");
+                        }
+                        else
+                        {
+                            File.AppendAllText(@"D:\KeysOnly.txt", character.KeyCode + "\r\n");
+                            File.AppendAllText(@"D:\ActiveProgram.txt", activeWindow.GetTitleOfActiveWindow() + "\r\n");
+                        }
+
+                        
                     }
 
+
                 });
+
             }
             base.OnStartup(e);
         }
